5c9004f0c9589f2bbb93317adb118bdb260bf263
augustoda1 <augustoda@al.insper.edu.br>
2026-04-10 10:04:16 -0300

    exe5

--- exe5/main.c
+++ exe5/main.c
@@ -23,24 +23,119 @@ const int BTN_PIN_Y = 21;
 const int LED_PIN_R = 5;
 const int LED_PIN_Y = 10;
 
+QueueHandle_t xQueueButId;
+SemaphoreHandle_t xSemaphore_r;
+QueueHandle_t xQueueButId2;
+SemaphoreHandle_t xSemaphore_y;
 
-void btn_task(void* p) {
-
-    while (true) {
-
+void btn_callback(uint gpio, uint32_t events) {
+    if (events == 0x4) { // fall edge
+        if(gpio == BTN_PIN_R){
+            xSemaphoreGiveFromISR(xSemaphore_r, 0);
+        }
+        else if(gpio == BTN_PIN_Y){
+            xSemaphoreGiveFromISR(xSemaphore_y, 0);
+        }
     }
 }
 
+void led_1_task(void *p) {
+    gpio_init(LED_PIN_R);
+    gpio_set_dir(LED_PIN_R, GPIO_OUT);
+
+    int estado = 0;
+    int delay;
+    while (true) {
+        if (xQueueReceive(xQueueButId, &delay, 0)) {
+            estado = !estado;
+        }
+        if (estado) {
+            gpio_put(LED_PIN_R, 1);
+            vTaskDelay(pdMS_TO_TICKS(100));
+            gpio_put(LED_PIN_R, 0);
+            vTaskDelay(pdMS_TO_TICKS(100));
+        }
+
+       else{
+            vTaskDelay(pdMS_TO_TICKS(10));
+       }
+    }
+}
+
+void btn_1_task(void* p) {
+    gpio_init(BTN_PIN_R);
+    gpio_set_dir(BTN_PIN_R, GPIO_IN);
+    gpio_pull_up(BTN_PIN_R);
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
+                                       &btn_callback);
+    
+    int delay = 100;
+
+    while (true) {
+        if (xSemaphoreTake(xSemaphore_r, pdMS_TO_TICKS(500)) == pdTRUE) {
+            printf("delay btn %d \n", delay);
+            xQueueSend(xQueueButId, &delay, 0);
+        }
+    }
+}
+void led_2_task(void *p) {
+    gpio_init(LED_PIN_Y);
+    gpio_set_dir(LED_PIN_Y, GPIO_OUT);
+
+    int estado = 0;
+    int delay;
+    while (true) {
+        if (xQueueReceive(xQueueButId2, &delay, 0)) {
+            estado = !estado;
+        }
+        if (estado) {
+            gpio_put(LED_PIN_Y, 1);
+            vTaskDelay(pdMS_TO_TICKS(100));
+            gpio_put(LED_PIN_Y, 0);
+            vTaskDelay(pdMS_TO_TICKS(100));
+        }
+
+       else{
+            vTaskDelay(pdMS_TO_TICKS(10));
+       }
+    }
+}
+
+void btn_2_task(void* p) {
+    gpio_init(BTN_PIN_Y);
+    gpio_set_dir(BTN_PIN_Y, GPIO_IN);
+    gpio_pull_up(BTN_PIN_Y);
+    gpio_set_irq_enabled(BTN_PIN_Y, GPIO_IRQ_EDGE_FALL, true);
+    
+    int delay = 100;
+
+    while (true) {
+        if (xSemaphoreTake(xSemaphore_y, pdMS_TO_TICKS(500)) == pdTRUE) {
+            printf("delay btn %d \n", delay);
+            xQueueSend(xQueueButId2, &delay, 0);
+        }
+    }
+}
 
 
 int main() {
     stdio_init_all();
 
-    xTaskCreate(btn_task, "BTN_Task 1", 256, NULL, 1, NULL);
+    xQueueButId = xQueueCreate(32, sizeof(int));
+    xSemaphore_r = xSemaphoreCreateBinary();
+
+    xQueueButId2 = xQueueCreate(32, sizeof(int));
+    xSemaphore_y = xSemaphoreCreateBinary();
+
+    xTaskCreate(led_1_task, "LED_Task 1", 256, NULL, 1, NULL);
+    xTaskCreate(btn_1_task, "BTN_Task 1", 256, NULL, 1, NULL);
+
+    xTaskCreate(led_2_task, "LED_Task 2", 256, NULL, 1, NULL);
+    xTaskCreate(btn_2_task, "BTN_Task 2", 256, NULL, 1, NULL);;
+
 
     vTaskStartScheduler();
 
-    while(1){}
+    while(true){}
 
-    return 0;
 }
